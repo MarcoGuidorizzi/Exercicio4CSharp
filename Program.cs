7ad4d631f23b8502aaff324efacf8c01acdb57e0
MARCO ANTONIO GUIDORIZZI COUTINHO DE SOUZA <62554313+MarcoGuidorizzi@users.noreply.github.com>
2026-04-07 16:09:23 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Exercicio4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int N, horas, resto; ;
+
+            N = int.Parse(Console.ReadLine());
+
+            horas = N / 3600;
+            resto = (N % 3600) / 60;
+
+            Console.WriteLine("{0}:{1}:{2}", horas, resto, N % 60);
+
+
+            Console.ReadLine();
+        }
+    }
+}
